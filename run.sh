6c42a603a9f8735b2267ead4a99808452c2a78ae
Zhiyun0411 <16785288+Zhiyun0411@users.noreply.github.com>
2019-11-03 17:53:56 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ for i in "${mainmajorVec[@]}"
 do
   majorVec="[$i]"
   paraDictPath="/Users/jessicaren/Downloads/research/Project04/03 myCode/organizeCode/paraDict/paraDict-K3-decay-2-l2-2-l1-2.json"
-  #. run-mf.sh
-  #. run-ck.sh
-  #. run-tr-mf.sh
-  #. run-tr-ck.sh
-  . run-ale.sh
-  #. run-ckcc.sh
-  #. run-ncf.sh
+  ./run-mf.sh
+  ./run-ck.sh
+  ./run-tr-mf.sh
+  ./run-tr-ck.sh
+  ./run-ale.sh
+  ./run-ckcc.sh
+  ./run-ncf.sh
 done
